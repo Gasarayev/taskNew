ec42b5871fab9bae72bfefc6dc12317d15cd5b51
Gasarayev <gasarayev.emil@gmail.com>
2024-07-15 18:06:05 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+// function createCounter() {
+//     var count = 0;
+
+//     return function() {
+//         count++;
+//         console.log(count);
+//         return count;
+//     };
+// }
+
+// const counter = createCounter();
+
+// counter();
+// counter();
+// counter();
+
+function useState(value) {
+  var newValue = value;
+  
+  function state() {
+    return value;
+  }
+
+  function setState(newValue) {
+    value = newValue;
+  }
+
+  return [state, setState];
+}
+
+var [someVar, setSomeVar] = useState(0);
+
+setSomeVar();
+console.log(someVar());
+
+setSomeVar(1);
+console.log(someVar());
+
+setSomeVar(6);
+console.log(someVar());
+
+setSomeVar(8);
+console.log(someVar());
+
+setSomeVar(9);
+console.log(someVar());
+
+// ---------------------------
+function addToBasket() {}
